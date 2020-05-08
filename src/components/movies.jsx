726fbdf4c4e25a0711f7804eaace23eadbf781dc
qiannyqian <liang.huiqian@gmail.com>
2020-05-08 17:38:43 +0800

    Added lodash, implement pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Like from '../components/common/like';
-
+import Pagination from '../components/common/pagination';
 import { getMovies } from '../services/fakeMovieService';
+import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -22,10 +25,17 @@ class Movies extends Component {
     this.setState({ movies: movies });
   };
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0) return <p>There are no movies in the database</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
       <div>
@@ -42,7 +52,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td> {movie.genre.name}</td>
@@ -66,6 +76,13 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+          currentPage={currentPage}
+        />
       </div>
     );
   }
