d1a47331166804b183ff53d92b803fd026d6e8e9
qiannyqian <liang.huiqian@gmail.com>
2020-05-11 14:30:55 +0800

    extract method from movies component, destructure arguments in common components

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 import TableHeader from '../common/tableHeader';
 import TableBody from '../common/tableBody';
 
-const Table = (props) => {
-  const { columns, sortColumn, onSort, data } = props;
-
+const Table = ({ columns, sortColumn, onSort, data }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
