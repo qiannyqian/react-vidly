f5a90b2a7056a09f736ec806c1d5650e365c569b
qiannyqian <liang.huiqian@gmail.com>
2020-05-12 14:30:32 +0800

    Added routing and new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
 import './App.css';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/movies" />
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
