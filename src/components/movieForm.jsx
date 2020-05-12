5244781a4328489672096b3bfd7cfda64b2911aa
qiannyqian <liang.huiqian@gmail.com>
2020-05-12 15:01:49 +0800

    Added navbar

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const MovieForm = () => {
-  return <h1>Movie Form</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form </h1>
+      {match.params.id}
+      <br />
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push('/movies')}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Table from '../components/common/table';
 import Like from '../components/common/like';
 
 class MoviesTable extends Component {
   columns = [
-    { path: 'title', label: 'Title' },
+    {
+      path: 'title',
+      label: 'Title',
+      content: (movie) => (
+        <Link Link to={`/movies/${movie._id}`}>
+          {movie.title}
+        </Link>
+      ),
+    },
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
