f8d01d623a4ea90fa984f83590537f10d4892e7f
qiannyqian <liang.huiqian@gmail.com>
2020-05-13 18:31:42 +0800

    extract form out into common component

---
+++ src/components/common/form.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import Input from './input';
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.data, this.schema, options);
+
+    if (!error) return null;
+
+    const errors = {};
+
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
+
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const errors = this.validate();
+
+    // if there are no errors, validate method will return null
+    this.setState({ errors: errors || {} });
+    //if there are errors, we will call them immediately
+    if (errors) return;
+
+    this.doSubmit();
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const data = { ...this.state.data };
+
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) {
+      errors[input.name] = errorMessage;
+    } else {
+      delete errors[input.name];
+    }
+
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderInput(name, label, type = 'text') {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+}
+
+export default Form;
